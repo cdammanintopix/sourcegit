04836fa981c79a7d329cf4925c0a6632fd7274b3
Corentin Damman <c.damman@intopix.com>
2025-09-10 11:13:26 +0200

    [CreateBranch] Always autocomplete branch name

--- src/ViewModels/CreateBranch.cs
+++ src/ViewModels/CreateBranch.cs
@@ -70,7 +70,7 @@ namespace SourceGit.ViewModels
             _repo = repo;
             _baseOnRevision = branch.Head;
 
-            if (!branch.IsLocal && repo.Branches.Find(x => x.IsLocal && x.Name == branch.Name) == null)
+            if (!branch.IsLocal /*&& repo.Branches.Find(x => x.IsLocal && x.Name == branch.Name) == null*/)
                 Name = branch.Name;
 
             BasedOn = branch;
